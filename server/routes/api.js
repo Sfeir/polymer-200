d6247fcdca3a9ebf2e2eb5ba37013511889e3b1c
cbalit <dou4872>
2016-06-17 16:58:58 +0200

    update feature

--- server/routes/api.js
+++ server/routes/api.js
@@ -83,8 +83,7 @@ exports.update = function (req, res) {
     return res.status(404).json({error: 'La personne avec l\'id "' + id + '" n\'existe pas.'});
   }
 
-  PEOPLES.splice(index, 1);
-  PEOPLES.push(person);
+  PEOPLES[index]=person;
 
   return res.status(200).json(person);
 };
